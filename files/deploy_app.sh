95fa73eaf744432548b52910f281ae9b0b9e7dd2
Tom Duffield <tom@chef.io>
2020-11-17 13:32:57 -0600

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. There needs to be more cats in this world.
   <!-- END -->
 
   </div>
